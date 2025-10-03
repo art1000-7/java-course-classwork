7763103c8c26eb26dcefb1db5422d567043d077d
art <popajopa215@gmail.com>
2025-10-03 23:50:06 +0600

    Finished

---
+++ ReverseNumbers.java
@@ -0,0 +1,18 @@
+package Shrava.ClassWorks;
+
+class Solution {
+    public int reverse(int x) {
+        int result = 0;
+        boolean isNegative = x < 0;
+        String strX = String.valueOf(Math.abs(x));
+        StringBuilder sb = new StringBuilder(strX).reverse();
+
+        try {
+            result = Integer.parseInt(sb.toString());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+        
+        return isNegative ? -result : result;  
+    }
+}
